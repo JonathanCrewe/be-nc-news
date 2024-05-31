258f34f1a4c017af29e1590f4798fcdf2e357cce
jonathancrewe <jonathancrewe@bellis-rogers.co.uk>
2024-05-31 15:32:32 +0100

    Q16 Routers

--- app.js
+++ app.js
@@ -4,28 +4,9 @@ const apiRouter = require("./routes/api-router")
 const {getCommentsByArticleId, postComment, deleteComment} = require('./controllers/comments.controller.js')
 const {getUsers} = require('./controllers/users.controller.js')
 
-// Set up. 
+
 const app = express()
-
-
-// End points. 
-
-// Api. 
 app.use('/api', apiRouter)
-
-
-
-
-
-
-
-
-// Comments.
-app.delete('/api/comments/:comment_id', deleteComment)
-
-
-// Users. 
-app.get('/api/users', getUsers)
 
 
 // Error handlers. 
@@ -38,5 +19,6 @@ app.use((err, req, res, next)=>{
         res.status(err.status).send({msg: err.msg})
     } else next(err)
 })
+
 
 module.exports = app
--- routes/api-router.js
+++ routes/api-router.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const apiRouter = require('express').Router()
+
 const {getEndpoints} = require('../controllers/endpoints.controller')
+
 const topicsRouter = require('./topics-router')
-const articlesRouter = require('./articles-router');
+const articlesRouter = require('./articles-router')
+const commentsRouter = require('./comments-router')
+const usersRouter = require('./users-router')
 
 apiRouter.use(express.json())
 
@@ -12,6 +16,8 @@ apiRouter.get('/', getEndpoints)
 // Endpoints. 
 apiRouter.use('/topics', topicsRouter)
 apiRouter.use('/articles', articlesRouter)
+apiRouter.use('/comments', commentsRouter)
+apiRouter.use('/users', usersRouter)
   
 
 module.exports = apiRouter;
